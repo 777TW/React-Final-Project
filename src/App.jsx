9c7511021f9baa3433efa143d12ab4e65b76ef3c
777TW <777happy666@gmail.com>
2026-05-28 21:06:14 +0800

    feat: add all classic map MapCards

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import './App.css'
 import NavBar from './components/NavBar.jsx'
 import MapFilterBar from './components/MapFilterBar.jsx';
+import MapCard from './components/MapCard.jsx';
+import mapData from './data/maps.json';
 
 function App() {
     return (
         <div>
             <NavBar />
             <MapFilterBar />
+
+            <div className="card-grid-container">
+                {mapData.map((mapInfo) => (
+                    <MapCard 
+                        key={mapInfo.id}
+                        imageSrc={mapInfo.imageSrc}
+                        title={mapInfo.title}
+                        difficultyText={mapInfo.difficultyText}
+                        activeBars={mapInfo.activeBars}
+                    />
+                ))}
+            </div>
         </div>
     );
 }
