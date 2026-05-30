5156fbdb1e4ab7d79f6c4c3cabb93318d67d12e4
777TW <777happy666@gmail.com>
2026-05-30 23:19:19 +0800

    style: change correct location pin to black

--- src/components/SummaryMap.jsx
+++ src/components/SummaryMap.jsx
@@ -21,6 +21,15 @@ function FitBounds({ correctLocation, guessedLocation }) {
     return null;
 }
 
+const blackIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
 export default function SummaryMap({ correctLocation, guessedLocation }) {
     return (
         <MapContainer 
@@ -34,7 +43,7 @@ export default function SummaryMap({ correctLocation, guessedLocation }) {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            {correctLocation && <Marker position={correctLocation} />}
+            {correctLocation && <Marker position={correctLocation} icon={blackIcon} />}
             {guessedLocation && <Marker position={guessedLocation} />}
             
             {correctLocation && guessedLocation && (
